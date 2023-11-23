4ce0914566d6b6b1cf661e1be5a159f4e0cc88eb
SancosJak <iskander.sancosjak@gmail.com>
2023-11-23 13:05:18 +0100

    update

---
+++ HomeWorks/lesson_8/hw2/script.js
@@ -0,0 +1,26 @@
+// // Задание 2
+// // Используя пройденные на занятии методы массива получите из этого массива новый массив, 
+// в котором элементы идут в обратной последовательности.
+
+let arrNum = [1, 2, 3, 4, 5];
+console.log("Исходный массив: " + arrNum);
+
+let arrNum2 = arrNum.reverse();
+console.log("В обратной последовательности: " + arrNum2);
+
+let arrStr = ["a", "b", "c", "d", "e"];
+console.log("Исходный массив: " + arrStr);
+
+let arrStr2 = arrStr.reverse();
+console.log("В обратной последовательности: " + arrStr2);
+
+let namesAndAges = [
+    'Семен 18 лет/годов',
+    'Иван 27 лет/годов',
+    'Петр 74 лет/годов',
+    'Татьяна 34 лет/годов'
+  ]
+console.log("Исходный массив: " + namesAndAges);
+
+let namesAndAges2 = namesAndAges.reverse();
+console.log("В обратной последовательности: " + namesAndAges2);
