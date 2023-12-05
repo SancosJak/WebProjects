f522c13b86cd73accc9e342bc568f34b2c39bc94
SancosJak <iskander.sancosjak@gmail.com>
2023-12-05 19:35:57 +0100

    update

---
+++ HomeWorks/lesson_13/script.js
@@ -0,0 +1,18 @@
+// Задание 1
+// Создайте метод getStudent, который возвращает promise - объекта (студент с именем "Anri" и возрастом 32), c задержкой 4 секунды.
+// Вызовите этот метод - сохраните в переменную.
+// Раскройте promise при помощи then, выведите имя студента.
+
+function getStudent() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ name: "Anri", age: 32 });
+        }, 4000);
+    });
+}
+
+let studentPromise = getStudent();
+
+studentPromise.then((student) => {
+    console.log(student.name);
+})
