6b0bc32364363184ecf4247ef829ed6e7cfc84fa
SancosJak <iskander.sancosjak@gmail.com>
2024-01-08 22:31:55 +0100

    update

---
+++ ClassWorks/lesson_22/NEWR/redux-intro/src/features/counter/Counter.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../../store';
+
+export default function Counter(): JSX.Element {
+    const dispatch = useDispatch();
+
+    function handlePlus(): void {
+        dispatch({ type: 'counter/plus', payload: 1 });
+    }
+
+    function handleMinus(): void {
+        dispatch({ type: 'counter/minus', payload: 1 }); // Исправлено здесь
+    }
+
+    const counter = useSelector((state: RootState) => state.counter.value);
+
+    return (
+        <div>
+            <p>Counter Value: {counter}</p>
+            <button type="button" onClick={handlePlus}>+</button>
+            <button type="button" onClick={handleMinus}>-</button>
+        </div>
+    );
+}
---
+++ ClassWorks/lesson_22/NEWR/redux-intro/src/features/counter/types/CounterState.ts
@@ -0,0 +1,5 @@
+export default interface CounterState {
+    value: number;
+}
+// Это тип нашего централизованого состояния.
+// может быть объектом, может быть массивом
