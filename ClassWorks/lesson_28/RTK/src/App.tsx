be144e70e50da3d3a1ad0d511290b3665470a846
SancosJak <iskander.sancosjak@gmail.com>
2024-01-22 19:28:12 +0100

    update

---
+++ ClassWorks/lesson_28/RTK/src/App.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App(): JSX.Element {
+	return <Routes></Routes>;
+}
+
+export default App;
